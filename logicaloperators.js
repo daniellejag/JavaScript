e59e2558a5ed0a5266a3677ec74cd2c582d5e621
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-24 23:57:33 -0400

    Update logicaloperators.js

--- logicaloperators.js
+++ logicaloperators.js
@@ -1,4 +1,17 @@
-   let country = prompt("What country are you from?");
+     let country = prompt("What country are you from?");
+        if (country === "Brazil" || country === "BRAZIL" || country === "brasil" || country === "brazil" || country === "Portugal" || country === "PORTUGAL" || country === "portugal" || country === "pORTugal") {
+            alert ("You speak Portuguese");
+        }else{
+            alert ("You don't speak Portuguese");
+        }  
+
+
+
+// 
+
+
+
+let country = prompt("What country are you from?");
         if (country === "Brazil" || country === "BRAZIL" || country === "brasil" || country === "Portugal" || country === "PORTUGAL" || country === "portugal" || country === "pORTugal") {
             alert ("You speak Portuguese");
         }else{
