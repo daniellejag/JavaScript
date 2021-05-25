f1976d0e2b4c82e0222e8498174c0961d14112de
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-24 23:54:18 -0400

    Create ifagefunctions.js

---
+++ ifagefunctions.js
@@ -0,0 +1,9 @@
+  let age = prompt ("How old are you?");
+        
+        if (age >= 18) {
+            alert ("You can apply to SheCodes");
+        }
+
+        if (age <= 17) {
+        alert ("You cannot apply to SheCodes ☹️");
+        }   
