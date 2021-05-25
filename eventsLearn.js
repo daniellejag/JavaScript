5f8972a20f7fb72ffc8acc2874ae26f22788855e
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-25 00:01:22 -0400

    Create eventsLearn.js

---
+++ eventsLearn.js
@@ -0,0 +1,8 @@
+function stopClick(event) {
+    event.preventDefault();
+let answer = prompt("Why do you want to learn Events?");
+alert("Ok we'll be in touch");
+}
+
+let link = document.querySelector("#event-link");
+heading.addEventListener("click", stopClick);
