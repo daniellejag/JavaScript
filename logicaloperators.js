2d37db71ee31bed697b3e546f4f2ef1ad9a6e1f1
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-24 23:56:39 -0400

    Create logicaloperators.js

---
+++ logicaloperators.js
@@ -0,0 +1,5 @@
+   let country = prompt("What country are you from?");
+        if (country === "Brazil" || country === "BRAZIL" || country === "brasil" || country === "Portugal" || country === "PORTUGAL" || country === "portugal" || country === "pORTugal") {
+            alert ("You speak Portuguese");
+        }else{
+            alert ("You don't speak Portuguese");
