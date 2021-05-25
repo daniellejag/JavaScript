149cb88a728d846054c4c4da6e557d15ec34578b
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-24 23:54:44 -0400

    Create ifelse.js

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+let time = prompt("What is the current hour?");
+
+        if (time < "12") {
+        alert ("Good morning ☕️🥞");
+        } else {
+            if (time >= "18") {
+                alert ("Good evening 🛋🍕");
+        } else {
+            alert ("Good afternoon ☀️");
+        }
+        }
