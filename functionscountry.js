dca24598519a04797a0bb96e6e31ed118059b150
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-24 23:53:38 -0400

    Create functionscountry.js

---
+++ functionscountry.js
@@ -0,0 +1,13 @@
+ function getCountry() {
+        let country = prompt("What country are you from?");
+        country = country.toLowerCase();
+        country = country.trim();
+        country = country.replace("brasil", "brazil");
+        if (country === "portugal" || country === "brazil") {
+          alert("You speak Portuguese!");
+        } else {
+          alert("Go learn Portuguese!");
+        }
+      }
+        
+        getCountry();
