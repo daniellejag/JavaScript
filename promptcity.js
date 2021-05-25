274e84b918cf6c291c225b057ba5150bc2d0a010
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-24 23:51:14 -0400

    Create promptcity.js

---
+++ promptcity.js
@@ -0,0 +1,9 @@
+  let hometown = prompt("What is your hometown?");
+        let Davie = "You are from Davie";
+        alert (Davie);
+        
+        let firstName = prompt("What is your first name?");
+        let Danielle = prompt("What is your last name?");
+        
+        let Jagendorf = "that's a funny last name";
+        alert (Jagendorf);
