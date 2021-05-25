750bb1d614d56d28d4bab2b2dcf20b55dcc3dfff
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-24 23:10:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,15 @@
-let weather = {
-  "paris": {
-    temp: 19.7,
-    humidity: 80
-  },
-  "tokyo": {
-    temp: 17.3,
-    humidity: 50
-  },
-  "lisbon": {
-    temp: 30.2,
-    humidity: 20
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  "moscow": {
-    temp: -5,
-    humidity: 20
-  }
-};
+let apiKey = "b3a8312e8813d91f8ac0edd65adaa9c3";
+let city = "Sydney";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
 
-//Include the JavaScript file provided in your project,  when loading your project, ask the user "Enter a city" (example: Paris), alert "It is currently 19°C (66°F) in Paris with a humidity of 80%"If the city doesn't exist in the object (i.e: Sydney), alert "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney". Since this is an alert, the link shouldn't be clickable.
+function showTemperature(response) {
+    let temperature = Math.round(response.data.main.temp);
+    let temperatureElement = document.querySelector("#temperature");
+    let description = document.querySelector("#temperature-description");
+    temperatureElement.innerHTML= `${temperature}℃`;
+    description.innerHTML = response.data.weather[0].description;
+}
+let h1 = document.querySelector("#city");
+h1.innerHTML = city;
 
-
-let userCity = prompt(`Enter a city`);
-
-
-  
-  if(weather[userCity] !== undefined) {
-    alert(`It is currently ${weather[userCity].temp}°C (66°F) in ${userCity} with a humidity of ${weather[userCity].humidity}%`);
-  } else {
-    alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${userCity}`);
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
