5085e50b11d2fd89e711c3c3f7d2908037389a0f
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-24 23:52:01 -0400

    Create alertcityprompt.js

---
+++ alertcityprompt.js
@@ -0,0 +1,20 @@
+let city = prompt("what is your hometown?");
+       alert (city);
+       
+       let lastName = prompt("what is your first name?");
+       alert (lastName);
+
+       let add = "from ";
+       
+       let firstName = prompt("what is your last name?");
+       alert (lastName + " " + firstName + " " + "from " + city);
+
+       let chipotle = prompt("...so would you like guac with that");
+
+       let lol = "that'll cost extra 🌯 😉 ";
+       
+       let blah = "(";
+       
+       let nowWhat = "I'm hoping you said yes because I don't know how to code for a no)";
+      
+       alert (lol + "(" + lastName + " " + nowWhat);
