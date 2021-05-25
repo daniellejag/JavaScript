41966bc75fff58d3fa17917356d79ee8d547d4d3
Danielle Jagendorf <77253082+daniellejag@users.noreply.github.com>
2021-05-25 00:03:13 -0400

    Create functionreturns.js

---
+++ functionreturns.js
@@ -0,0 +1,16 @@
+function isWindy(speed, unit) {
+if (speed > 5 && unit === "metric") {
+return true;
+} else { 
+return false
+}
+} 
+
+let speed = prompt("what is the wind speed");
+let unit = prompt("In what unit? (metric or imperial?)");
+
+if (isWindy(speed)) {
+alert("It is windy");
+} else { 
+alert("It is not windy");
+} 
